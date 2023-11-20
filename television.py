58fca760199d6dc28470eec68a110949b73cb3fa
YossR21 <yrafael@unomaha.edu>
2023-11-20 13:49:58 -0600

    Added File

--- television.py
+++ television.py
@@ -11,8 +11,8 @@ class Television:
         # Instance variables (private)
         self._status = False
         self._muted = False
-        self._volume = self.MIN_VOLUME
-        self._channel = self.MIN_CHANNEL
+        self._volume = Television.MIN_VOLUME
+        self._channel = Television.MIN_CHANNEL
 
     def power(self):
         self._status = not self._status
@@ -20,25 +20,25 @@ class Television:
     def mute(self):
         self._muted = not self._muted
         if self._muted:
-            self._volume = self.MIN_VOLUME
+            self._volume = Television.MIN_VOLUME
 
     def channel_up(self):
         if self._status:
-            self._channel = (self._channel + 1) % (self.MAX_CHANNEL + 1)
+            self._channel = (self._channel + 1) % (Television.MAX_CHANNEL + 1)
 
     def channel_down(self):
         if self._status:
-            self._channel = (self._channel - 1) % (self.MAX_CHANNEL + 1)
+            self._channel = (self._channel - 1) % (Television.MAX_CHANNEL + 1)
 
     def volume_up(self):
         if self._status:
             self._muted = False  # Unmute if volume is adjusted
-            self._volume = min(self._volume + 1, self.MAX_VOLUME)
+            self._volume = min(self._volume + 1, Television.MAX_VOLUME)
 
     def volume_down(self):
         if self._status:
             self._muted = False  # Unmute if volume is adjusted
-            self._volume = max(self._volume - 1, self.MIN_VOLUME)
+            self._volume = max(self._volume - 1, Television.MIN_VOLUME)
 
     def __str__(self):
         return f"Power = [{self._status}], Channel = [{self._channel}], Volume = [{self._volume}]"
