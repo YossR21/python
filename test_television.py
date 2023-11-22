efb5d7e52f1a4aa2e53f6ed9550e63298c6421f3
YossR21 <151274326+YossR21@users.noreply.github.com>
2023-11-21 20:01:34 -0600

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -1,8 +1,9 @@
-#import pytest
 from television import Television
+import pytest
 
+tv = Television()
 def test_init():
-    tv = Television()
+    # tv = Television()
     assert str(tv) == "Power = False, Channel = 0, Volume = 0"
 
 def test_power():
@@ -14,27 +15,14 @@ def test_power():
     assert str(tv_off) == "Power = False, Channel = 0, Volume = 0"
 
 def test_mute():
-    tv = Television()
-    tv.power()
-    tv.volume_up()
+    # tv = Television()
+    tv.power()  # Turn on the TV
+    tv.volume_up()  # Increase volume once
     tv.mute()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
-
-    tv.unmute()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
-
-    tv.power()
+    assert tv.muted is True
+    assert tv.volume_down() == Television.MIN_VOLUME  # Volume should be reset when muted
     tv.mute()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
-
-    tv.power()
-    tv.unmute()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
-
-    tv.power()
-    tv.mute()
-    tv.power()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 1"
+    assert tv.mute() is False
 
 def test_channel_up():
     tv_off = Television()
@@ -118,3 +106,6 @@ def test_volume_down():
     tv_on_min_volume.volume_down()
     tv_on_min_volume.volume_down()
     assert str(tv_on_min_volume) == "Power = True, Channel = 0, Volume = 0"
+
+if __name__ == '__main__':
+    pytest.main()
